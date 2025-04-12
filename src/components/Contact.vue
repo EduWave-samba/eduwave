319d26232b5a7054023953c72d126f0952bc335d
eduwave <eduwavesamba@gmail.com>
2025-04-12 17:35:43 +0530

    fixes

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -27,7 +27,7 @@
                         <a class="btn btn-block email-button" href="mailto:eduwavesamba@gmail.com?subject=Enquiry for Tuition Fee from Website&body=Hi, I’d like to know more about your tuition services."><i class="material-icons">email</i>Email Us</a>
                     </div>
                 </div>
-                <p><i class="material-icons left">location_on</i>EduWave, 1st Floor, Near ISKCON, Near Hospital Road, National Highway, Samba, J&K</p>
+                <p><i class="material-icons left">location_on</i>EduWave, 1st Floor, Care Health Insurance, Near Hospital Road, National Highway, Samba, J&K</p>
             </div>
             <div class="video-container col s12 m6" style="margin-top: 20px; height:420px">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d210.16993606578552!2d75.11917904857012!3d32.56034457354678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391c228e275f489d%3A0x84571966b3c68b65!2sNIIT%20Samba%20Centre!5e0!3m2!1sen!2sin!4v1744390923406!5m2!1sen!2sin" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
